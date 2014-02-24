34678e22b02784e2074d0006852bb286249852d3
Kevin-Chung <kvnchung92@gmail.com>
2014-02-24 12:32:06 -0600

    Created SignUp activity to be used by organizations to signup for the app itself. Needs some error checking done on the entries, and needs to query parse DB to make sure duplicates aren't found.

--- gen/com/example/signmeups/R.java
+++ gen/com/example/signmeups/R.java
@@ -24,23 +24,36 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
-        public static final int button1=0x7f08000b;
+        public static final int OrgEmail=0x7f080013;
+        public static final int OrgName=0x7f080012;
+        public static final int OrgPassword=0x7f080014;
+        public static final int action_settings=0x7f080017;
+        public static final int button1=0x7f080005;
+        public static final int button2=0x7f080006;
+        public static final int button3=0x7f080007;
+        public static final int button4=0x7f080008;
+        public static final int cancel=0x7f080016;
         public static final int editText1=0x7f080001;
         public static final int editText2=0x7f080002;
-        public static final int editText3=0x7f080004;
+        public static final int editText3=0x7f080009;
         public static final int login_button=0x7f080003;
-        public static final int radio3=0x7f080007;
-        public static final int radio4=0x7f080008;
-        public static final int radio5=0x7f080009;
-        public static final int radio6=0x7f08000a;
-        public static final int radioButton2=0x7f080006;
-        public static final int radioGroup1=0x7f080005;
+        public static final int mailList=0x7f080010;
+        public static final int radio3=0x7f08000c;
+        public static final int radio4=0x7f08000d;
+        public static final int radio5=0x7f08000e;
+        public static final int radio6=0x7f08000f;
+        public static final int radioButton2=0x7f08000b;
+        public static final int radioGroup1=0x7f08000a;
+        public static final int signUpButton=0x7f080015;
+        public static final int signUpText=0x7f080011;
+        public static final int signup_button=0x7f080004;
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
-        public static final int front_end=0x7f030001;
+        public static final int admin_panel=0x7f030001;
+        public static final int front_end=0x7f030002;
+        public static final int sign_up=0x7f030003;
     }
     public static final class menu {
         public static final int login=0x7f070000;
--- src/com/example/signmeups/FrontEnd.java
+++ src/com/example/signmeups/FrontEnd.java
@@ -1,12 +1,33 @@
 package com.example.signmeups;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FrontEnd extends Activity {
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override
+	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.front_end);
+        
+        
+
+        Button loginButton = (Button)findViewById(R.id.button1);
+    
+        loginButton.setOnClickListener(new View.OnClickListener(){
+    	@Override
+		public void onClick(View view){
+    		Intent intent = new Intent(FrontEnd.this, Login.class);
+    		startActivity(intent);
+    		
+    	}
+    });
     }
+    
+    
+    
+    
 
 }
